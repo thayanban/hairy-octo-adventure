31f6e77c550540c336084d1bfe5cfec676dfa73e
thayanban <thayanban@gmail.com>
2013-10-18 14:22:57 +0530

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+exports.login = function(req, res) {
+	res.render('login');
+};
+
+exports.register = function(req, res) {
+	res.render('register');
+};
+
+exports.createUser = function(req, res) {
+	res.render('register');
+	var user = req.body
+	;
+	//save user to db
+	//redirect to login. 
+};
